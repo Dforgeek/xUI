89dc5038a1ee538b8baaec98f72faf0703db1990
StRom42 <stromanenko10@gmail.com>
2025-09-06 13:33:06 +0300

    fix schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,33 +5,56 @@ create table user_info
     post int not null,
     command_id bigint not null
 );
-
 create index user_info_telegram_id_idx on user_info (telegram_id);
 create index user_info_command_id_idx on user_info (command_id);
+
+create table block
+(
+    id bigserial primary key,
+    block_name varchar(255) not null
+);
+
+create table question
+(
+    id bigserial primary key,
+    block_id bigint not null,
+    question_text text not null,
+    question_type smallint not null,
+    answer_fields text not null
+);
+create index question_block_id_idx on question (block_id);
 
 create table survey
 (
     id bigserial primary key,
-    questions text not null,
+    subject_user_id bigint not null,
     created_at timestamp with time zone not null,
-    deadline timestamp with time zone not null
+    deadline timestamp with time zone not null,
+    notifications_before bigint not null
 );
 
-create table survey_respondents
+create table survey_question
 (
     id bigserial primary key,
-    survey_id bigint not null,
-    user_id bigint not null
+    question_id bigint not null,
+    survey_id bigint not null
 );
+create unique index survey_question_survey_id_question_id_idx on survey_question (question_id, survey_id);
 
-create unique index survey_respondents_survey_id_user_id on survey_respondents (survey_id, user_id);
+create table survey_respondent
+(
+    id bigserial primary key,
+    user_id bigint not null,
+    survey_id bigint not null
+);
+create unique index survey_respondent_user_id_survey_id_idx on survey_respondent (user_id, survey_id);
 
-create table survey_answers
+create table survey_answer
 (
     id bigserial primary key,
     survey_id bigint not null,
     user_id bigint not null,
+    question_id bigint not null,
     answer text not null
 );
-
-create index survey_answers_survey_id_user_id on survey_answers (survey_id, user_id);
+create index survey_answer_survey_id_question_id_idx on survey_answer (survey_id, question_id);
