5c09c2cdb7d1a941ed9608684bccff303364b68f
Xozyain <74548499+Xozyain@users.noreply.github.com>
2025-09-08 20:55:34 +0300

    Add req. Fix init_model

--- llm/init_model.py
+++ llm/init_model.py
@@ -1,3 +1,7 @@
+import os
 from langchain.chat_models import init_chat_model
+
+if not os.environ.get("DEEPSEEK_API_KEY"):
+  os.environ["DEEPSEEK_API_KEY"] = ""
 
 model = init_chat_model("deepseek-chat", model_provider="deepseek")
